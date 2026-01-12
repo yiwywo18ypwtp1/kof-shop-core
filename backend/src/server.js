b16ae9ffb117f5b2494a13ec0974f5af030d2c07
yiwywo18ypwtp1 <negalapetrovna77@gmail.com>
2026-01-12 04:03:45 +0100

    chore: add docker-compose for frontend, backend and db

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,6 @@ import app from './app.js'
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`API running on http://localhost:${PORT}`);
 });
